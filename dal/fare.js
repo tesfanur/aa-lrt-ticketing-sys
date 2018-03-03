60012ea0b485681f43a50a1aa4c8fdd5aa17c2d5
Tesfaye Belachew <get.tesfaye.belachew@gmail.com>
2018-03-03 10:40:42 +0300

    done again

--- dal/fare.js
+++ dal/fare.js
@@ -117,8 +117,8 @@ function getAllFares(query){
     debug('GETTING ALL FARE COLLECTION');
     var defferd =q.defer();
  FareModel.find(query)
-          .populate('from','name stationId route')
-          .populate('to','name stationId route')
+          // .populate('from','name stationId route')
+          // .populate('to','name stationId route')
           .populate('userId','email phone userType')
           .sort({createdAt:-1})
           .exec()
